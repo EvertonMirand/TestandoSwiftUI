d4af9230104d05b38f779b611a4936bf2ea4bfa2
EvertonMirand <everton.mirandav@gmail.com>
2020-11-05 18:28:11 -0300

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Everton Miranda Vitório on 05/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
