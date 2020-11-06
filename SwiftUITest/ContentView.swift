03a44e57885746cbd98ff7c802404ba05f1cbcc6
EvertonMirand <everton.mirandav@gmail.com>
2020-11-05 21:18:11 -0300

    Cria projeto

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -7,11 +7,43 @@
 
 import SwiftUI
 
+final class DataModel: ObservableObject {
+  static let shared = DataModel()
+  @Published var string = "This is an ObservedObject string"
+}
+
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    
+    @ObservedObject var data = DataModel.shared
+      let substring: Substring = "This is a substring"
+      let string = "This is a string"
+      var body: some View {
+        VStack {
+          //This is a substring
+          Text(substring)
+          
+          //This is a string
+          Text(string)
+          
+          //This is an ObservedObject string
+          Text(data.string)
+          
+          //string_key
+          Text(verbatim: "string_key")
+          
+          //This string in the default file
+          Text("string_key")
+          
+          //This string is in another file
+          Text("string_key", tableName: "Local", bundle: Bundle.main, comment: "Comment")
+            HStack {
+                Text("oi")
+                Text("oi")
+            }
+        }
+
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
